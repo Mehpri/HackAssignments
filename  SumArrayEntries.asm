9600bb16da1ab55854928cf5bd51a1524bdccbc6
Malachi <gqxd5106@leeds.ac.uk>
2025-03-25 22:00:05 +0000

    done

---  SumArrayEntries.asm
+++  SumArrayEntries.asm
@@ -1,63 +1,49 @@
 @R1
 D=M
 @STORE_ZERO
-D;JLE       // If R1 ≤ 0, store 0 in R2 and exit
+D;JLE
 
-// Initialize sum = 0
-@sum
+@R2
 M=0
 
-// Store array starting address in R3 (to avoid modifying R0)
 @R0
 D=M
 @R3
-M=D         // R3 = base address of array
+M=D
 
-// Store counter in R4 (to avoid modifying R1)
 @R1
 D=M
 @R4
-M=D         // R4 = number of elements
+M=D
 
 (LOOP)
 @R4
 D=M
 @END_LOOP
-D;JLE       // If counter ≤ 0, exit loop
+D;JEQ
 
-// Load the current array value
 @R3
-A=M         // Load memory at address stored in R3
-D=M         // D = current element
-@sum
-M=M+D       // sum += current element
+A=M
+D=M
+@R2
+M=M+D
 
-// Move to the next element (increment pointer)
 @R3
-M=M+1       // R3 = R3 + 1 (move to next array item)
+M=M+1
 
-// Decrement counter
 @R4
 M=M-1
 
 @LOOP
-0;JMP       // Repeat until counter = 0
-
-(END_LOOP)
-@sum
-D=M
-@STORE
 0;JMP
 
-// Case where R1 was ≤ 0, store 0 in R2
 (STORE_ZERO)
 @0
 D=A
-
-(STORE)
 @R2
-M=D         // Store the computed sum in R2
+M=D
 
+(END_LOOP)
 (END)
 @END
-0;JMP 
+0;JMP    
