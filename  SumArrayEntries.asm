5410f5495bf702794804fb33790c9941e472d9b9
Malachi <gqxd5106@leeds.ac.uk>
2025-03-25 21:53:13 +0000

    done

---  SumArrayEntries.asm
+++  SumArrayEntries.asm
@@ -1,47 +1,63 @@
+// Check if R1 (length) is ≤ 0
 @R1
 D=M
-@ZERO_CASE
-D;JLE      // Handle length <= 0 case
+@STORE_ZERO
+D;JLE       // If length ≤ 0, return 0
 
+// Initialize pointer to array start (R0)
 @R0
 D=M
 @ptr
-M=D        // Initialize pointer to array start
+M=D         // ptr = array base address
+
+// Store counter in a separate register (R3)
 @R1
 D=M
-@n
-M=D        // Initialize counter
+@counter
+M=D         // counter = R1 (number of elements)
+
+// Initialize sum to 0
 @sum
-M=0        // Initialize sum
+M=0
 
 (LOOP)
-@ptr
-A=M        // Get current array address
-D=M        // Load current element
-@sum
-M=M+D      // Add to sum
-
-@ptr
-M=M+1      // Move to next element
-@n
-M=M-1      // Decrement counter
+@counter
 D=M
-@LOOP
-D;JGT      // Continue if counter > 0
+@END_LOOP
+D;JLE       // If counter ≤ 0, exit loop
 
+// Load current array value
+@ptr
+A=M         // Load array address
+D=M         // D = current element
+@sum
+M=M+D       // sum += current element
+
+// Move to next element
+@ptr
+M=M+1       // ptr++
+
+// Decrement counter
+@counter
+M=M-1
+
+@LOOP
+0;JMP       // Repeat until counter = 0
+
+(END_LOOP)
 @sum
 D=M
 @STORE
 0;JMP
 
-(ZERO_CASE)
+(STORE_ZERO)
 @0
-D=A
+D=A         // Set sum = 0 if R1 ≤ 0
 
 (STORE)
 @R2
-M=D        // Store result
+M=D         // Store sum result in R2
 
 (END)
 @END
-0;JMP      // Infinite loop
+0;JMP       // Infinite loop
