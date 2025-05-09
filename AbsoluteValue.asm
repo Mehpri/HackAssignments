aae219a45fd7f77b4a55c459228c4c6b3a4607e5
Malachi <gqxd5106@leeds.ac.uk>
2025-05-09 14:38:00 +0000

    redone with comments

--- AbsoluteValue.asm
+++ AbsoluteValue.asm
@@ -1,43 +1,48 @@
+//Code to calculate the absolute value of a 16 bit number
+// Firstly R0 is loaded and is checked if it's positive
 @R0
 D=M
 @positive
-D;JGE
+D;JGE // If x>= 0 code will jump to the 'positive' part of the code
 
+// If the code isn't positive it must mean that it's negative so will carry onto the code that handles negative cases
 @R2
-M=1
-
+M=1  // Negative flag is set
 @32768    
-D=D+A
+D=D+A // D = R0 + 32768 to make sure that R0 isn't equal to -32768 (as positive 32768 can't be represented in 16 bit format)
 @overflow
-D;JEQ
+D;JEQ // If D = 0 it means R0 is equal to -32768 and code will jump to the 'overflow' portion of the code
 
+//Computes the absolute value for negative numbers that don't have an overflow
 @R0
-D=M
-D=-D
+D=M  // Reloads original 16 bit number
+D=-D // Makes the negative number positive
 @R3
-M=0
-@store
-0;JMP
+M=0  // No overflow
+@store 
+0;JMP // Jumps to store result
 
 (overflow)
 @R3
-M=1
+M=1 // Overflow detected
 @R0
-D=M
+D=M // Reloads original 16 bit number
 @store
-0;JMP
+0;JMP // Jumps to store result
 
 (positive)
 @R2
-M=0
+M=0  // R0 is positive
 @R3
-M=0
+M=0  // No overflow
 @R0
-D=M
+D=M // Original number is the absolute value of that number
 
 (store)
 @R1
-M=D
+M=D // stores absolute value or original number if there was an overflow
+
+//Halts program
 (end)
 @end
 0;JMP
--- Xor.asm
+++ Xor.asm
@@ -1,20 +1,28 @@
+// Simplified version of previous code so no boolean algebra has to be used just means code is slightly longer
+// XOR = (x ∧ ¬y) V (¬x ∧ y)
+// First we will do the logic for (x ∧ ¬y)
+@R1         
+D=!M        // D = ¬y
 @R0
-D=M     
-@R1
-D=D|M    
+D=D&M       // D = x ∧ ¬y
 @temp
-M=D      
+M=D         // temp = x ∧ ¬y
 
+// Next the logic for (¬x ∧ y)
 @R0
-D=M      
+D=!M        // D = ¬x
 @R1
-D=D&M    
-D=!D     
+D=D&M       // D = ¬x ∧ y
 
+// Combine (x ∧ ¬y) V (¬x ∧ y) 
 @temp
-D=D&M    
+D=D|M       // D = (¬x ∧ y) ∨ (x ∧ ¬y)
+
+// Store result in R2
 @R2
-M=D      
+M=D         // R2 = XOR
+
+// End program 
 (end)
 @end
 0;JMP
